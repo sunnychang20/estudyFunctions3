abb80394bd0442d34faa9f528f3df246b579fb16
SunnyC <sunnychang20@gmail.com>
2023-11-26 01:43:17 +0800

    5
    5

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,14 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.android.library") version "8.1.4" apply false
 }
+
+publishing {
+    publications {
+        create("maven_public", MavenPublication::class) {
+            groupId = "org.gradle.sample"
+            artifactId = "library"
+            version = "1.1"
+//            from(components.getByName("java"))
+        }
+    }
+}
--- estudyFunctions1/build.gradle.kts
+++ estudyFunctions1/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:*")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -41,3 +41,4 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
